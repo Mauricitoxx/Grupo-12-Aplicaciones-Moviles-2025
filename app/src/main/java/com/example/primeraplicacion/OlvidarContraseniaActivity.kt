848a4123c0a4d0be45d4f35ea855947c9bc1f90f
Sofia.Raggi <sofiraggi26@gmail.com>
2025-04-08 14:24:21 -0300

    Layout y Activity en caso de "Olvide mi contraseña"

---
+++ app/src/main/java/com/example/primeraplicacion/OlvidarContraseniaActivity.kt
@@ -0,0 +1,11 @@
+package com.example.primeraplicacion
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class OlvidarContraseniaActivity : AppCompatActivity() {
+}
